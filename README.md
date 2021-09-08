917652f5caf327e72c9d6bf86b5f647eb9d5af10
leandromliveira <leandrooliveira35@gmail.com>
2021-09-08 14:00:26 -0300

    subindo para o github

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1>Projeto Minha Lista de Tarefas</h1>
+<p>Na lista de tarefas você pode colocar as atividades que você deseja desempenhar durante o dia, caso tenha completado alguma, você pode clicar duas vezes sobre ela e ela ficará como completa, caso deseja mudar a posição basta selecionar a tarefa escolhida e clicar nos botões para mover para cima, baixo ou excluir. Também existe a possibilidade de salvar a lista para acompanhar o que não foi feito hoje para o dia seguinte.
+Você pode apagar todas as tarefas ou apagar somente as tarefas já completas</p>
+<img src="/images/todo-list.png" alt="minha lista de tarefas">
+<h2> Avaliação do projeto: <h2>
+
+<img src="/images/nota-projeto.png" alt="avaliação do projeto" />
---
+++ script.js
@@ -0,0 +1,92 @@
+const textoTarefa = document.getElementById("texto-tarefa");
+const criarTarefa = document.getElementById("criar-tarefa");
+const listaTarefas = document.getElementById("lista-tarefas");
+const apagaTudo = document.getElementById("apaga-tudo");
+const removerFinalizados = document.getElementById("remover-finalizados");
+const salvarTarefas = document.getElementById("salvar-tarefas");
+const removerSelecionado = document.getElementById("remover-selecionado");
+const moverCima = document.getElementById("mover-cima");
+const moverBaixo = document.getElementById("mover-baixo");
+
+if (localStorage.getItem("content")) {
+  listaTarefas.innerHTML = localStorage.getItem("content");
+}
+
+function taskAdd() {
+  const tarefa = document.createElement("li");
+  tarefa.innerText = textoTarefa.value;
+  tarefa.classList.add("tarefa");
+  listaTarefas.appendChild(tarefa);
+  textoTarefa.value = "";
+}
+
+function taskSelected(event) {
+  const selected = document.querySelector(".selected");
+  if (!selected) {
+    event.target.classList.add("selected");
+  } else {
+    selected.classList.remove("selected");
+    event.target.classList.add("selected");
+  }
+}
+
+function taskCompleted(event) {
+  if (event.target.classList.contains("completed")) {
+    event.target.classList.remove("completed");
+  } else {
+    event.target.classList.add("completed");
+  }
+}
+
+function removeAll() {
+  listaTarefas.innerHTML = "";
+}
+
+function removeFinished() {
+  const finished = document.querySelectorAll(".completed");
+  console.log(finished);
+  for (let index = 0; index < finished.length; index += 1) {
+    listaTarefas.removeChild(finished[index]);
+  }
+}
+
+function taskSave() {
+  localStorage.setItem("content", listaTarefas.innerHTML);
+}
+
+function removeSelected() {
+  const selected = document.querySelector(".selected");
+  if (selected) {
+    listaTarefas.removeChild(selected);
+  }
+}
+
+//codigo utilizado do site https://developer.mozilla.org/pt-BR/docs/Web/API/Node/insertBefore
+
+function moveUp() {
+  const elementSelected = document.getElementsByClassName("selected")[0];
+  const list = document.querySelectorAll(".tarefa");
+  if (list[0] !== elementSelected && elementSelected) {
+    const elementUp = elementSelected.previousElementSibling;
+    listaTarefas.insertBefore(elementUp, elementSelected.nextSibling);
+  }
+}
+
+function moveDown() {
+  const elementSelected = document.getElementsByClassName("selected")[0];
+  const list = document.querySelectorAll(".tarefa");
+  if (list[list.length - 1] !== elementSelected && elementSelected) {
+    const elementDown = elementSelected.nextSibling;
+    listaTarefas.insertBefore(elementDown, elementSelected);
+  }
+}
+
+criarTarefa.addEventListener("click", taskAdd);
+listaTarefas.addEventListener("click", taskSelected);
+listaTarefas.addEventListener("dblclick", taskCompleted);
+apagaTudo.addEventListener("click", removeAll);
+removerFinalizados.addEventListener("click", removeFinished);
+salvarTarefas.addEventListener("click", taskSave);
+removerSelecionado.addEventListener("click", removeSelected);
+moverCima.addEventListener("click", moveUp);
+moverBaixo.addEventListener("click", moveDown);
